8c4823e460e9d4bb7ff15f538feddf3fda5348ff
Cristian Perez <cperezlecaros@gmail.com>
2026-01-07 15:42:45 -0300

    Create application, company, user and job resources

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { CompanyModule } from './company/company.module';
+import { JobModule } from './job/job.module';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
-  imports: [PrismaModule],
+  providers: [AppService, PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UserModule,
+    CompanyModule,
+    JobModule,
+    ApplicationModule,
+  ],
 })
 export class AppModule {}
