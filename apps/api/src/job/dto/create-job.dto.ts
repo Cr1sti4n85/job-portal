64912d6c9879d4b473bce92b29b7dcb5b0458467
Cristian Perez <cperezlecaros@gmail.com>
2026-02-08 13:28:41 -0300

    Update job entity and creat-job dto

--- apps/api/src/job/dto/create-job.dto.ts
+++ apps/api/src/job/dto/create-job.dto.ts
@@ -1,1 +1,40 @@
-export class CreateJobDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateJobDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  requirements: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  salary: number;
+
+  @IsNotEmpty()
+  @IsString()
+  location: string;
+
+  @IsNotEmpty()
+  @IsString()
+  jobType: string;
+
+  @IsNotEmpty()
+  @IsString()
+  experienceLevel: string;
+
+  @IsNotEmpty()
+  @IsString()
+  companyId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  createdBy: string;
+}
